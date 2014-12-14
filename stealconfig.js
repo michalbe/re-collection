119d3a06bd14ac4a71f4c7a32758f28c7a4b95a4
Michał Budzyński <michal@virtualdesign.pl>
2014-12-14 01:01:00 +0100

    :fishing_pole_and_fish: stealconfig added

---
+++ stealconfig.js
@@ -0,0 +1,23 @@
+steal.config({
+  map: {
+    "jquery/jquery": "jquery"
+  },
+  paths: {
+    "steal/*" : "../*.js",
+    "@traceur": "traceur/traceur.js",
+    "can/*": "bower_components//CanJS/*.js",
+    "jquery": "bower_components//jquery/dist/jquery.js"
+  },
+  meta: {
+    jquery: {
+      exports: "jQuery",
+      format: "global"
+    }
+  },
+
+  ext: {
+    "mustache": "can/view/mustache/system"
+  }
+});
+
+System.buildConfig = {map: {"can/util/util" : "can/util/domless/domless"}};
