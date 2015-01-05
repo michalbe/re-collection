df53f570c77275be88803cd5babb658b98bd76fd
Michał Budzyński <michal@virtualdesign.pl>
2015-01-05 01:01:00 +0100

    :dress: render first chunk of data

---
+++ script/script.js
@@ -0,0 +1,20 @@
+var table = document.getElementById('table');
+
+var renderPosition = function(data) {
+  var parent = document.createElement('tr');
+
+  // Cover
+  var cover = document.createElement('td');
+  cover.style.backgroundImage = 'url(data/covers/no-cover.jpg)';
+
+  // Title
+  var title = document.createElement('td');
+  title.innerHTML = data.title;
+
+  parent.appendChild(cover);
+  parent.appendChild(title);
+
+  table.appendChild(parent);
+}
+
+renderPosition(collection[0]);
